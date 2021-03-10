a3ce273d53b0448b54487a2fa4f83c96c022995f
Nikolas North <nikolasn09@gmail.com>
2021-03-09 20:42:01 -0500

    Refactor Hello()

--- hello.go
+++ hello.go
@@ -10,23 +10,36 @@ const spanishWorldPostfix = "Mundo"
 const frenchWorldPostfix = "Monde"
 
 func Hello(name, language string) string {
-	prefix := englishHelloPrefix
-	postfix := englishWorldPostfix
-
-	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
-		postfix = spanishWorldPostfix
-	case french:
-		prefix = frenchHelloPrefix
-		postfix = frenchWorldPostfix
-	}
-
+	postfix := getPostfix(language)
 	if name != "" {
 		postfix = name
 	}
 
-	return prefix + postfix
+	return getPrefix(language) + postfix
+}
+
+func getPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func getPostfix(language string) (postfix string) {
+	switch language {
+	case spanish:
+		postfix = spanishWorldPostfix
+	case french:
+		postfix = frenchWorldPostfix
+	default:
+		postfix = englishWorldPostfix
+	}
+	return
 }
 
 func main() {
