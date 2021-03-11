e2e5f4e64dded566aa337f61061cfcb36f50ca91
Nikolas North <nikolasn09@gmail.com>
2021-03-10 21:23:11 -0500

    Change numbers type for Sum() to slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,6 @@
 package arrays
 
-// Returns the sum of all integers in numbers array
+// Returns the sum of all integers in numbers slice
 func Sum(numbers []int) (sum int) {
 	sum = 0
 	for _, number := range numbers {
