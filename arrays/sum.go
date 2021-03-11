eabdd4eb4fcd513107d7320e70e0f873beac362c
Nikolas North <nikolasn09@gmail.com>
2021-03-10 21:21:42 -0500

    Change numbers type for Sum() to slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,7 +1,7 @@
 package arrays
 
 // Returns the sum of all integers in numbers array
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	sum = 0
 	for _, number := range numbers {
 		sum += number
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -6,23 +6,25 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %d want %d, given %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d, given %q", got, want, numbers)
+		}
+	})
 }
 
 func ExampleSum() {
-	fmt.Println(Sum([5]int{1, 2, 3, 4, 5}))
+	fmt.Println(Sum([]int{1, 2, 3, 4, 5}))
 	// Output: 15
 }
 
 func BenchmarkSum(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Sum([5]int{1, 2, 3, 4, 5})
+		Sum([]int{1, 2, 3, 4, 5})
 	}
 }
