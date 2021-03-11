ed17301ab55fea8bcd0bf4d674cd7987e39ae098
Nikolas North <nikolasn09@gmail.com>
2021-03-11 18:40:29 -0500

    Implement SumAll()

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,3 +8,12 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+// Returns an integer array containing the summed values for every slice
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	sums = make([]int, len(numbersToSum))
+	for i, v := range numbersToSum {
+		sums[i] = Sum(v)
+	}
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,6 +2,7 @@ package arrays
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -26,5 +27,25 @@ func ExampleSum() {
 func BenchmarkSum(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Sum([]int{1, 2, 3, 4, 5})
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func ExampleSumAll() {
+	fmt.Println(SumAll([]int{1, 2}, []int{0, 9}))
+	// Output: [3 9]
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2}, []int{0, 9})
 	}
 }
