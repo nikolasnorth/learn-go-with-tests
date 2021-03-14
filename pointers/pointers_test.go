a2c94197db3cebbc7dcca197c0bed627f57565c5
Nikolas North <nikolasn09@gmail.com>
2021-03-14 19:20:16 -0400

    Implement Withdraw()

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -5,9 +5,19 @@ import "testing"
 func TestWallet(t *testing.T) {
 
 	t.Run("Deposit", func(t *testing.T) {
-		t.Helper()
 		w := Wallet{}
 		w.Deposit(Bitcoin(10))
+		got := w.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		w := Wallet{balance: Bitcoin(20)}
+		w.Withdraw(Bitcoin(10))
 		got := w.Balance()
 		want := Bitcoin(10)
 
