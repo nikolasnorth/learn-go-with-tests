6159348edff26ddd1fcb06f0027f3a24054663a5
Nikolas North <nikolasn09@gmail.com>
2021-03-14 21:08:20 -0400

    Add err handling to Withdraw()

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -1,28 +1,60 @@
 package pointers
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 
 	t.Run("Deposit", func(t *testing.T) {
 		w := Wallet{}
 		w.Deposit(Bitcoin(10))
-		got := w.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, w, want)
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw with funds", func(t *testing.T) {
 		w := Wallet{balance: Bitcoin(20)}
-		w.Withdraw(Bitcoin(10))
-		got := w.Balance()
+		err := w.Withdraw(Bitcoin(10))
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertNoError(t, err)
+		assertBalance(t, w, want)
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(0)
+		w := Wallet{startingBalance}
+		got := w.Withdraw(Bitcoin(10))
+		want := ErrInsufficientFunds
+
+		assertError(t, got, want)
+		assertBalance(t, w, startingBalance)
+	})
+}
+
+func assertBalance(t *testing.T, w Wallet, want Bitcoin) {
+	t.Helper()
+	got := w.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+	if got.Error() != want.Error() {
+		t.Errorf("got %q want %q", got.Error(), want)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("got an error but didn't want one")
+	}
 }
--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,11 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("insufficient funds")
 
 type Bitcoin int
 
@@ -12,8 +17,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if w.balance-amount < 0 {
+		return ErrInsufficientFunds
+	}
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
