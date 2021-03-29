58595eb4172d71e576949b41aca41221d8f9bdfc
Nikolas North <nikolasn09@gmail.com>
2021-03-28 20:21:51 -0400

    using select statement to wait on multiple channels

---
+++ select/racer.go
@@ -0,0 +1,33 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecTimeout = 10 * time.Second
+
+func Racer(url1, url2 string) (string, error) {
+	return ConfigurableRacer(url1, url2, tenSecTimeout)
+}
+
+func ConfigurableRacer(url1, url2 string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(url1):
+		return url1, nil
+	case <-ping(url2):
+		return url2, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", url1, url2)
+	}
+}
+
+func ping(url string) chan struct{} {
+	c := make(chan struct{})
+	go func() {
+		_, _ = http.Get(url)
+		close(c)
+	}()
+	return c
+}
---
+++ select/racer_test.go
@@ -0,0 +1,42 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compares speed of servers, returns fastest url", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		want := fastServer.URL
+		got, err := Racer(slowServer.URL, fastServer.URL)
+		if err != nil {
+			t.Fatalf("got an error but didn't expect one %v", err)
+		}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("returns error if server doesn't respond within specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
